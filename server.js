66b5af368ec913ace795f4aca419855b11a65463
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-10 17:45:21 -0400

    minor changes

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html')
 app.set('views', (__dirname + '/public'));
 
-mongoose.connect("mongodb://localhost/workout", {
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
